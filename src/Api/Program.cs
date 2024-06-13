a8bef35025b388a2167becfa6681d5d0d8bb1f49
GuiPozzane <gui_pozzani@hotmail.com>
2024-06-13 18:37:48 -0300

    fact - improvements in missing values in pipeline and changed Program.cs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -17,12 +17,8 @@ namespace Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
